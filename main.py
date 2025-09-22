6574301560e13f2ddc417749b317126d74e4311a
Andrei Chervov <JWTHDYTWA@yandex.ru>
2025-09-23 02:46:41 +0700

    Final fixes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
+from warnings import warn
 from transformers import pipeline, TextGenerationPipeline
+
 
 
 def text_pipeline_init(lm:str, padding:str):
@@ -25,7 +27,7 @@ def text_pipeline_init(lm:str, padding:str):
     return pipe
 
 
-def inference(style:str, input:str|list[str], pipe: TextGenerationPipeline, token_limit=None):
+def inference(style:str, input:str|list[str], pipe: TextGenerationPipeline, token_limit=None, len_limit=None):
     """
     Перефразирует входной текст в указанном стиле с использованием заданного пайплайна.
 
@@ -33,12 +35,15 @@ def inference(style:str, input:str|list[str], pipe: TextGenerationPipeline, toke
         style (str): Стиль, в котором необходимо перефразировать текст.
         input (str | list[str]): Строка или список строк для обработки.
         pipe (TextGenerationPipeline): Инициализированный пайплайн TextGenerationPipeline из библиотеки transformers.
+        token_limit (int): Ограничение на количество выходных токенов модели.
+        len_limit (int): Максимальная длина одной входной строки.
 
     Returns:
         list[str]: Список перефразированных текстов.
 
     Raises:
-        TypeError: Если параметр `input` не является строкой или списком строк.
+        TypeError: Если один из параметров не соответствует ожидаемому типу.
+        ValueError: Если функции передан пустой запрос.
     """
 
     message = None
@@ -52,8 +57,11 @@ def inference(style:str, input:str|list[str], pipe: TextGenerationPipeline, toke
     # Разные алгоритмы для обработки одиночной строки и списка строк
 
     if isinstance(input, str):
-        if input.strip() == '':
+        input = input.strip()
+        if input == '':
             raise ValueError('Входная строка не должна быть пустой или состоять только из пробелов.')
+        if len_limit is not None and len(input) > len_limit:
+            raise ValueError('Длина входной строки превышает максимально допустимый размер.')
         message = [
             {'role': 'system', 'content': f'Твоя задача — перефразировать в указанном стиле текст, который присылает пользователь. Не добавляй ничего от себя, даже кавычки.'},
             {'role': 'user', 'content': f'Текст: "{input}"\nСтиль: {style}'}
@@ -65,18 +73,23 @@ def inference(style:str, input:str|list[str], pipe: TextGenerationPipeline, toke
         for ln in input:
             if not isinstance(ln, str):
                 continue
-            elif ln.strip() == '':
-                # К сожалению, сложно оставить разделяющие строки
-                # чтобы сохранить структуру входного файла, поэтому
-                # они просто пропускаются.
-                continue
             else:
+                ln = ln.strip()
+                if ln == '':
+                    continue
+                elif len_limit is not None and len(ln) > len_limit:
+                    raise ValueError('Длина одной из входных строк превышает максимально допустимый размер.')
+                
                 message.append(
                     [
                         {'role': 'system', 'content': f'Твоя задача — перефразировать в указанном стиле текст, который присылает пользователь. Не добавляй ничего от себя, даже кавычки.'},
                         {'role': 'user', 'content': f'Текст: "{ln}"\nСтиль: {style}'}
                     ]
                 )
+                # К сожалению, сложно оставить разделяющие строки
+                # чтобы сохранить структуру входного файла, поэтому
+                # они просто пропускаются.
+                continue
         if not message:
             raise ValueError('Список запросов не должен быть пустым.')
         answer = pipe(message, batch_size=16, max_new_tokens=token_limit)
@@ -87,12 +100,16 @@ def inference(style:str, input:str|list[str], pipe: TextGenerationPipeline, toke
     
     return answer
 
+
 # Привязка к конкретной модели вызвана отличающимся форматом данных у разных моделей (проверено).
 # При переключении модели может сломаться индексация контейнеров, из-за чего потребуется переписывать код.
 # Примечание: Qwen3, в отличии от Gemma, не является Gated моделью и не требует токена HuggingFace
 MODEL_NAME = "Qwen/Qwen3-4B-Instruct-2507"
 DEFAULT_STYLE = "Официальный"
-MAX_TOKENS = 50
+MAX_OUTPUT_TOKENS = 50
+# Допустимая длина строки в целом зависит от контекстного окна модели,
+# так что я не стал добавлять её в качестве аргумента командной строки.
+MAX_INPUT_LENGTH = 200
 
 def main():
 
@@ -104,7 +121,7 @@ def main():
     parser.add_argument('-o', '--output', default='output.txt', help='Путь к выходному файлу. По умолчанию - "output.txt".')
     parser.add_argument('-r', '--realtime', action='store_true', help='Запуск в режиме реального времени.')
     parser.add_argument('-s', '--style', default=DEFAULT_STYLE, help=f'Выбор стиля, в котором будет переписан текст. По умолчанию - "{DEFAULT_STYLE}".')
-    parser.add_argument('-t', '--tokens', default=MAX_TOKENS, type=int, help=f'Лимит output-токенов на один запрос. Влияет на длину ответов. По умолчанию - {MAX_TOKENS}.')
+    parser.add_argument('-t', '--tokens', default=MAX_OUTPUT_TOKENS, type=int, help=f'Лимит output-токенов на один запрос. По умолчанию - {MAX_OUTPUT_TOKENS}.')
     args = parser.parse_args()
 
     # Инициализация пайплайна модели
@@ -129,7 +146,7 @@ def main():
                 continue
             else:
                 try:
-                    answer = inference(args.style, text, pipe_instance, args.tokens)
+                    answer = inference(args.style, text, pipe_instance, args.tokens, MAX_INPUT_LENGTH)
                     print(answer + '\n')
                 except Exception as e:
                     print(f'Ошибка обработки текста:\n{e}')
@@ -140,7 +157,7 @@ def main():
                 lines = f.read().splitlines()
 
             # Передача входных данных модели и получение ответа
-            answer = inference(args.style, lines, pipe_instance, args.tokens)
+            answer = inference(args.style, lines, pipe_instance, args.tokens, MAX_INPUT_LENGTH)
 
             with open(args.output, 'w', encoding='utf-8') as f:
                 f.write('\n'.join(answer))
